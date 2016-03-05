083d74aa38ad1e8031d3f00815b83c6e155957c5
SergeyS523 <sivagin@hotmail.com>
2016-03-05 22:16:36 +0300

    Добавил в CSS новую структуру страницы. Страница INDEX изменена.

---
+++ engine/hidden/themes_c/8da38ded3bad578c1f89cb110f301d346e0675dd_0.file.content.tpl.php
@@ -0,0 +1,62 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-03-05 22:14:40
+  from "D:\Web\OpenServer\domains\localhost\themes\todo\content.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_56db3020ad2c46_26755444',
+  'file_dependency' =>
+  array (
+    '8da38ded3bad578c1f89cb110f301d346e0675dd' =>
+    array (
+      0 => 'D:\\Web\\OpenServer\\domains\\localhost\\themes\\todo\\content.tpl',
+      1 => 1457205279,
+      2 => 'file',
+    ),
+  ),
+  'includes' =>
+  array (
+  ),
+),false)) {
+function content_56db3020ad2c46_26755444 ($_smarty_tpl) {
+?>
+<div class="info">
+    <div class="space">
+        <div class="smLeft">
+            <div class="panel">
+                <div class="panel-head">
+                    Последние изменения:
+                </div>
+                <div class="panel-content">
+                    Изменений нет &nbsp;<i class="fa fa-plus"></i>
+                </div>
+            </div>
+        </div>
+        <div class="smRight">
+            <div class="panel">
+                <div class="panel-head">
+                    Список проектов:
+                </div>
+                <div class="panel-content">
+                    Проектов не создано &nbsp;<i class="fa fa-plus"></i>
+                </div>
+            </div>
+        </div>
+        <div class="laCenter">
+            <div class="index">
+                <div class="ToDoPosition">
+                    <div class="panel">
+                        <div class="panel-head">
+                            Список задач на сегодня:
+                        </div>
+                        <div class="panel-content">
+                            Задач на сегодя нет &nbsp;<i class="fa fa-plus"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><?php }
+}
